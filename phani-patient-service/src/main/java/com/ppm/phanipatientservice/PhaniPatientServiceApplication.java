865492541941644ea7b62364f4fa788dc987aec4
phani bandaru <pbndru@gmail.com>
2025-03-19 07:24:34 +0000

    Initital commit

---
+++ phani-patient-service/src/main/java/com/ppm/phanipatientservice/PhaniPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ppm.phanipatientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhaniPatientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhaniPatientServiceApplication.class, args);
+    }
+
+}
